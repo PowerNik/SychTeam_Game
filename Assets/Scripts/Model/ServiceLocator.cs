7229a127aed61ae49151863387512dc3f4a8f077
nmaschinskiy <nmaschinskiy@mail.ru>
2018-06-23 21:21:39 +0400

    Created serviceLocator

---
+++ Assets/Scripts/Model/ServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ServiceType
+{
+	DialogSystem = 0,
+}
+
+public static class ServiceLocator
+{
+	private static Dictionary<ServiceType, MonoBehaviour> serviceDict = new Dictionary<ServiceType, MonoBehaviour>();
+
+	public static void SetService(ServiceType type, MonoBehaviour monobeh)
+	{
+		serviceDict[type] = monobeh;
+	}
+
+	public static MonoBehaviour GetService(ServiceType type)
+	{
+		return serviceDict[type];
+	}
+
+}
--- Assets/Scripts/Person/DialogActor.cs
+++ Assets/Scripts/Person/DialogActor.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof(Interactable))]
 public class DialogActor : MonoBehaviour
 {
-	[SerializeField]
-	private string[] phrases;
-
-	private int phraseNumber = 0;
-
 	void Start()
 	{
 		GetComponent<Interactable>().Interacted += StartDialog;
@@ -17,32 +12,10 @@ public class DialogActor : MonoBehaviour
 
 	private void StartDialog()
 	{
-		if (phraseNumber < phrases.Length)
+		var mono = ServiceLocator.GetService(ServiceType.DialogSystem);
+		if (GetComponent<Dialogues>())
 		{
-			Debug.Log(phrases[phraseNumber]);
-
-			if (phraseNumber + 1 < phrases.Length)
-			{
-				phraseNumber++;
-			}
+			((DialogueInteraction)mono).SetDialogue(GetComponent<Dialogues>());
 		}
 	}
-
-	/*private void OnTriggerEnter(Collider other)
-	{
-		if (other.GetComponent<Interactable>())
-		{
-			Debug.Log("Interact with " + other.name);
-			other.GetComponent<Interactable>().Interact();
-		}
-	}
-
-	private void OnCollisionEnter(Collision collision)
-	{
-		if (collision.collider.GetComponent<Interactable>())
-		{
-			Debug.Log("Interact with " + collision.collider.name);
-			collision.collider.GetComponent<Interactable>().Interact();
-		}
-	}*/
 }
