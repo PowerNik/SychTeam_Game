51d33e5d6f5bf7ce27cf09304dfe04c005d5fc60
DVMonk <dvmonakhov@yandex.ru>
2018-08-11 21:25:34 +0300

    Added diary scenes
    Prototype of reading diary.

---
+++ Assets/Scripts/Diary/NextPage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextPage : MonoBehaviour 
+{
+	public void GoToNextPage() 
+	{
+		if (SceneManager.GetActiveScene ().name == "Diary1")
+			SceneManager.LoadScene ("Diary2");
+		else if (SceneManager.GetActiveScene ().name == "Diary2")
+			SceneManager.LoadScene ("MyTest");
+	}
+}
