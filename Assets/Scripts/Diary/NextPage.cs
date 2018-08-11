a5fde7a9b9820e8b74045b665775a87cc88b483a
DVMonk <dvmonakhov@yandex.ru>
2018-08-11 21:52:41 +0300

    Changed diary system
    Using one scene for all pages instead one scene for one page.

--- Assets/Scripts/Diary/NextPage.cs
+++ Assets/Scripts/Diary/NextPage.cs
@@ -8,8 +8,12 @@ public class NextPage : MonoBehaviour
 	public void GoToNextPage() 
 	{
 		if (SceneManager.GetActiveScene ().name == "Diary1")
-			SceneManager.LoadScene ("Diary2");
-		else if (SceneManager.GetActiveScene ().name == "Diary2")
 			SceneManager.LoadScene ("MyTest");
 	}
+
+	public void GoToNextPage(string sceneName) 
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+		
 }
