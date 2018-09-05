72cf51bb722d7de9f8a23a472084e9d5d9ed9578
nmaschinskiy <nmaschinskiy@mail.ru>
2018-09-05 22:27:49 +0300

    added QuestSender.cs

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
 	public event System.Action Interacted;
+    public UnityEvent unityEvent;
 
-	public void Interact()
-	{
-		if (Interacted != null)
-		{
-			Interacted();
-		}
-	}
+    public void Interact()
+    {
+        Interacted?.Invoke();
+        unityEvent?.Invoke();
+    }
 }
---
+++ Assets/Scripts/Quests/QuestSender.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestSender : MonoBehaviour
+{
+    [SerializeField]
+    private List<QuestState> states = new List<QuestState>();
+
+    [ContextMenu("SendQuests")]
+    public void SendQuests()
+    {
+        ServiceLocator.QuestSystem.SetQuestProgress(states);
+        gameObject.SetActive(false);
+    }
+}
