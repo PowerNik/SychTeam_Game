0470c1ff6added4502d380244542258429ea4f70
nmaschinskiy <nmaschinskiy@mail.ru>
2018-09-05 23:10:49 +0300

    minor refactored of some quest scripts

--- Assets/Scripts/Quests/QuestCondition.cs
+++ Assets/Scripts/Quests/QuestCondition.cs
@@ -7,28 +7,24 @@ public enum LogicOperation { And, Or }
 [System.Serializable]
 public class QuestCondition
 {
-	public List<QuestState> states = new List<QuestState>();
-	public List<LogicOperation> logics = new List<LogicOperation>();
+    public List<QuestState> states = new List<QuestState>();
+    public List<LogicOperation> logics = new List<LogicOperation>();
 
-	private List<int> orConditionList = new List<int>();
-	private List<List<int>> andConditionList = new List<List<int>>();
+    private List<int> orConditionList = new List<int>();
+    private List<List<int>> andConditionList = new List<List<int>>();
 
-	public bool IsTrue
-	{
+    public bool IsTrue
+    {
         get { return GetConditionValue(); }
-	}
+    }
 
     private bool GetConditionValue()
     {
         if (states.Count == 0 || ServiceLocator.QuestSystem == null)
-        {
             return true;
-        }
 
         if (states.Count == 1)
-        {
             return ServiceLocator.QuestSystem.CheckQuestState(states[0]);
-        }
 
         orConditionList.Clear();
         andConditionList.Clear();
@@ -39,114 +35,91 @@ public class QuestCondition
         ProcessLastValue();
 
         if (CheckOrConditions())
-        {
             return true;
-        }
         else
-        {
             return CheckAndConditions();
-        }
     }
 
 
     private void ProcessFirstValue()
-	{
-		if (logics.Count == 0)
-		{
-			return;
-		}
+    {
+        if (logics.Count == 0)
+            return;
 
-		if (logics[0] == LogicOperation.And)
-		{
-			andConditionList[0].Add(0);
-		}
-		else
-		{
-			if (logics[0] == LogicOperation.Or)
-			{
-				orConditionList.Add(0);
-			}
-		}
-	}
+        if (logics[0] == LogicOperation.And)
+            andConditionList[0].Add(0);
+        else
+        {
+            if (logics[0] == LogicOperation.Or)
+                orConditionList.Add(0);
+        }
+    }
 
-	private void ProcessMiddleValues()
-	{
-		for (int i = 1; i < logics.Count - 1; i++)
-		{
-			if (logics[i] == LogicOperation.And)
-			{
-				andConditionList[andConditionList.Count - 1].Add(i + 1);
-			}
+    private void ProcessMiddleValues()
+    {
+        for (int i = 1; i < logics.Count - 1; i++)
+        {
+            if (logics[i] == LogicOperation.And)
+                andConditionList[andConditionList.Count - 1].Add(i + 1);
 
-			if (logics[i] == LogicOperation.Or)
-			{
-				if (logics[i + 1] == LogicOperation.Or)
-				{
-					orConditionList.Add(i + 1);
-				}
-				if (logics[i + 1] == LogicOperation.And)
-				{
-					andConditionList.Add(new List<int>());
-					andConditionList[andConditionList.Count - 1].Add(i + 1);
-				}
-			}
-		}
-	}
+            if (logics[i] == LogicOperation.Or)
+            {
+                if (logics[i + 1] == LogicOperation.Or)
+                    orConditionList.Add(i + 1);
 
-	private void ProcessLastValue()
-	{
-		if (logics.Count == 0)
-		{
-			return;
-		}
+                if (logics[i + 1] == LogicOperation.And)
+                {
+                    andConditionList.Add(new List<int>());
+                    andConditionList[andConditionList.Count - 1].Add(i + 1);
+                }
+            }
+        }
+    }
 
-		int lastLogic = logics.Count - 1;
-		if (logics[lastLogic] == LogicOperation.And)
-		{
-			andConditionList[andConditionList.Count - 1].Add(lastLogic + 1);
-		}
-		else
-		{
-			if (logics[lastLogic] == LogicOperation.Or)
-			{
-				orConditionList.Add(lastLogic + 1);
-			}
-		}
-	}
+    private void ProcessLastValue()
+    {
+        if (logics.Count == 0)
+            return;
 
-	private bool CheckOrConditions()
-	{
-		for (int index = 0; index < orConditionList.Count; index++)
-		{
-			if(ServiceLocator.QuestSystem.CheckQuestState(states[index]))
-			{
-				return true;
-			}
-		}
+        int lastLogic = logics.Count - 1;
+        if (logics[lastLogic] == LogicOperation.And)
+            andConditionList[andConditionList.Count - 1].Add(lastLogic + 1);
+        else
+        {
+            if (logics[lastLogic] == LogicOperation.Or)
+                orConditionList.Add(lastLogic + 1);
+        }
+    }
 
-		return false;
-	}
+    private bool CheckOrConditions()
+    {
+        for (int index = 0; index < orConditionList.Count; index++)
+        {
+            if (ServiceLocator.QuestSystem.CheckQuestState(states[index]))
+                return true;
+        }
 
-	private bool CheckAndConditions()
-	{
-		foreach(var andCond in andConditionList)
-		{
-			bool result = true;
-			for (int i = 0; i < andCond.Count; i++)
-			{
-				if (ServiceLocator.QuestSystem.CheckQuestState(states[andCond[i]]) == false)
-				{
-					result = false;
-					break;
-				}
-			}
+        return false;
+    }
 
-			if(result)
-			{
-				return true;
-			}
-		}
+    private bool CheckAndConditions()
+    {
+        foreach (var andCond in andConditionList)
+        {
+            bool result = true;
+            for (int i = 0; i < andCond.Count; i++)
+            {
+                if (ServiceLocator.QuestSystem.CheckQuestState(states[andCond[i]]) == false)
+                {
+                    result = false;
+                    break;
+                }
+            }
 
-		return false;
-	}
+            if (result)
+                return true;
+        }
+
+        return false;
+    }
 }
--- Assets/Scripts/Quests/QuestSystem.cs
+++ Assets/Scripts/Quests/QuestSystem.cs
@@ -6,82 +6,87 @@ using UnityEngine;
 //[CreateAssetMenu(fileName = "QuestSystem", menuName = "QuestSystem")]
 public class QuestSystem : ScriptableObject
 {
-	public List<QuestState> questList = new List<QuestState>();
+    [SerializeField]
+    private List<QuestState> questList = new List<QuestState>();
 
-	public void SetQuestProgress(QuestType type, QuestProgress progress)
-	{
-		QuestState questPair = questList.Find(pair => pair.questType == type);
-		if (questPair == null)
-		{
-			AddNewQuest(type, progress);
-		}
-		else
-		{
-			UpdateQuestProgress(questPair, progress);
-		}
-	}
+    private List<QuestSubscriber> subscribers = new List<QuestSubscriber>();
 
-	public void SetQuestProgress(List<QuestState> list)
-	{
-		foreach(var state in list)
-		{
-			SetQuestProgress(state.questType, state.questProgress);
-		}
-	}
+    public void Subscribe(QuestSubscriber subscriber)
+    {
+        subscribers.Add(subscriber);
+    }
 
-	public QuestProgress GetQuestProgress(QuestType type)
-	{
-		QuestState result = questList.Find(pair => pair.questType == type);
-		if (result == null)
-		{
-			return QuestProgress.Not_started;
-		}
+    public void Unsubscribe(QuestSubscriber subscriber)
+    {
+        subscribers.Remove(subscriber);
+    }
 
-		return result.questProgress;
-	}
 
-	public bool CheckQuestState(QuestState state)
-	{
-		QuestState result = questList.Find(pair => pair.questType == state.questType);
-		if (result == null)
-		{
-			return QuestProgress.Not_started == state.questProgress;
-		}
+    public bool CheckQuestState(QuestState state)
+    {
+        QuestState result = questList.Find(pair => pair.questType == state.questType);
+        if (result == null)
+            return QuestProgress.Not_started == state.questProgress;
 
-		return result.questProgress == state.questProgress;
-	}
+        return result.questProgress == state.questProgress;
+    }
 
-	private void AddNewQuest(QuestType type, QuestProgress progress)
-	{
-		if (progress == QuestProgress.Started || progress == QuestProgress.Failed)
-		{
-			questList.Add(new QuestState(type, progress));
-		}
-		else
-		{
-			Debug.Log(String.Format(
-				"Try to ADD quest '{0}' with progress '{1}'",
-				type, progress));
-		}
-	}
+    public List<bool> CheckQuestState(List<QuestState> states)
+    {
+        List<bool> result = new List<bool>();
+        foreach (var state in states)
+            result.Add(CheckQuestState(state));
 
-	private void UpdateQuestProgress(QuestState questPair, QuestProgress progress)
-	{
-		if (questPair.questProgress <= progress)
-		{
-			questPair.questProgress = progress;
-		}
-		else
-		{
-			Debug.Log(String.Format(
-				"Tried to UPDATE quest '{0}' ({1}) by progress '{2}'",
-				questPair.questType, questPair.questProgress, progress));
-		}
-	}
+        return result;
+    }
 
-	[ContextMenu("RemoveAllQuests")]
-	private void RemoveAllQuests()
-	{
-		questList.Clear();
-	}
+
+    public void SetQuestProgress(List<QuestState> list)
+    {
+        foreach (var state in list)
+            SetQuestProgress(state.questType, state.questProgress);
+
+        for (int i = subscribers.Count - 1; i >= 0; i--)
+            subscribers[i].QuestsUpdated();
+    }
+
+    private void SetQuestProgress(QuestType type, QuestProgress progress)
+    {
+        QuestState questPair = questList.Find(pair => pair.questType == type);
+        if (questPair == null)
+            AddNewQuest(type, progress);
+        else
+            UpdateQuest(questPair, progress);
+    }
+
+    private void AddNewQuest(QuestType type, QuestProgress progress)
+    {
+        if (progress == QuestProgress.Started || progress == QuestProgress.Failed)
+            questList.Add(new QuestState(type, progress));
+        else
+        {
+            Debug.Log(String.Format(
+                "Try to ADD quest '{0}' with progress '{1}'",
+                type, progress));
+        }
+    }
+
+    private void UpdateQuest(QuestState questPair, QuestProgress progress)
+    {
+        if (questPair.questProgress <= progress)
+            questPair.questProgress = progress;
+        else
+        {
+            Debug.Log(String.Format(
+                "Tried to UPDATE quest '{0}' ({1}) by progress '{2}'",
+                questPair.questType, questPair.questProgress, progress));
+        }
+    }
+
+
+    [ContextMenu("ResetQuests")]
+    private void ResetQuests()
+    {
+        questList.Clear();
+    }
 }
